3ce5ab6d31fd0802080cc82c20c792a77e97877c
Divya Rao <divya1aum@gmail.com>
2018-03-20 15:42:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT bears.name, bears.age FROM bears where bears.temperament == 'GOOFY';"
+  "SELECT count(bears.temperament) FROM bears where bears.temperament == 'GOOFY';"
 end
 
 def selects_bear_that_killed_Tim
