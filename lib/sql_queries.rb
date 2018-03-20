af3d91feea769e3cd3d988f960b635f63baf6b73
Divya Rao <divya1aum@gmail.com>
2018-03-20 16:03:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,11 +11,11 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-   "SELECT bears.name, bears.age from bears where max(age);"
+   "SELECT bears.name, bears.age from bears order by bears.age DESC limit 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age from bears where min(age);"
+  "SELECT bears.name, bears.age from bears order by bears.age ASC limit 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
